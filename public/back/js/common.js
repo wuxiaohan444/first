463ea6b0fb079ff7dca3ce340de44ce72a60f8ba
WH <a624396832@outlook.com>
2017-10-29 21:49:49 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,49 @@
 /**
  * Created by 吴晗 on 2017/10/29 0029.
  */
+if (location.href.indexOf("login.html") < 0) {
+    $.ajax({
+        type: "get",
+        url: "/employee/checkRootLogin",
+        success: function (data) {
+            if (data.error === 400) {
+                location.href = "login.html";
+            }
+        }
+    })
+}
+$(function () {
+    $(document).ajaxStart(function () {
+        NProgress.start();
+    })
+    $(document).ajaxStop(function () {
+        setTimeout(function () {
+            NProgress.done();
+        }, 500);
+    })
+});
+
+$(function () {
+    $(".child").prev().on("click", function () {
+        $(this).next().slideToggle();
+    });
+    $(".icon_menu").on("click", function () {
+        $(".lt_aside").toggleClass("now");
+        $(".main").toggleClass("now");
+    })
+
+    $(".icon_logout").on("click", function () {
+        $("#logoutModal").modal("show");
+    });
+    $(".btn_logout").on("click", function () {
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function (data) {
+                if(data.success){
+                    window.location.href = "login.html";
+                }
+            }
+        })
+    })
+});
